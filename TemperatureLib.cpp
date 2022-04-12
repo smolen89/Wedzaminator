f45878e6a62a3ad820945dab7af6c284203e8a2d
Ebbi Gebbi <smolen89@gmail.com>
2022-04-12 07:05:02 +0200

    Wyodrębnienie Czujników DT

--- TemperatureLib.cpp
+++ TemperatureLib.cpp
@@ -6,7 +6,42 @@ TemperatureLib::TemperatureLib(uint8_t sensors_pin)
     _sensors.setOneWire(&_ow);
     _sensors.setResolution(TEMPERATURE_SENSOR_PRECISION);
     _sensors.begin();
-    refresh();
+
+    Serial.println("========================");
+    Serial.println("Dallas Temperature Sensors Initialize");
+    Serial.println("========================");
+
+    Serial.print("Rozdzielczość: ");
+    Serial.println(_sensors.getResolution());
+
+    Serial.print("Wykrytych urządzeń: ");
+    Serial.println(_sensors.getDeviceCount());
+
+    // call sensors.requestTemperatures() to issue a global temperature
+    // request to all devices on the bus
+    Serial.print("Requesting temperatures...");
+    _sensors.requestTemperatures(); // Send the command to get temperatures
+    Serial.println("DONE");
+
+    // After we got the temperatures, we can print them here.
+    // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+    for ( int DeviceIndex = 0; DeviceIndex < _sensors.getDeviceCount(); DeviceIndex++ )
+    {
+        float tempC = _sensors.getTempCByIndex(DeviceIndex);
+
+        // Check if reading was successful
+        if (tempC != DEVICE_DISCONNECTED_C)
+        {
+            Serial.print("Temperature for the device ");
+            Serial.print(DeviceIndex);
+            Serial.print(" is: ");
+            Serial.println(tempC, TEMPERATURE_SENSOR_DISMENSION);
+        }
+        else
+        {
+            Serial.println("Error: Could not read temperature data");
+        }
+    }
 }
 
 void TemperatureLib::refresh()
@@ -67,4 +102,10 @@ void TemperatureLib::Search()
     Serial.println("========================");
     Serial.println("Dallas Temperature Sensor Log");
     Serial.println("========================");
+
+}
+
+DallasTemperature TemperatureLib::GetDT()
+{
+    return _sensors;
 }
