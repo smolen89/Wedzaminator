eeb84fbbbb1d87107df8b8a01f65178ffe9c4a13
Ebbi Gebbi <smolen89@gmail.com>
2022-04-12 22:39:29 +0200

    Dodano Obsługę Timerów

---
+++ Timers.cpp
@@ -0,0 +1,51 @@
+#include "Timers.h"
+
+void Timer::begin(const uint32_t interval)
+{
+    time(interval);
+    restart();
+}
+
+void Timer::restart()
+{
+    _lastTime = millis();
+}
+
+bool Timer::available()
+{
+    if (_time == 0)
+    {
+        return false;
+    }
+
+    uint32_t actualTime = millis();
+    uint32_t deltaTime = actualTime - _lastTime;
+
+    if (deltaTime >= _time)
+    {
+        return true;
+    }
+
+    return false;
+}
+void Timer::add(const uint32_t amount)
+{
+    _time += amount;
+}
+uint32_t Timer::time()
+{
+    if (_time == 0)
+    {
+        return 0;
+    }
+
+    uint32_t actualTime = millis();
+    uint32_t deltaTime = actualTime - _lastTime;
+
+    return _time - deltaTime;
+}
+
+void Timer::time(const uint32_t interval)
+{
+    _time = interval;
+}
---
+++ Timers.h
@@ -0,0 +1,27 @@
+#ifndef _Timers_h
+    #define _Timers_h
+
+    #include <Arduino.h>
+    #include <inttypes.h>
+
+    #define SECS(t) (unsigned long) (t * 1000)
+    #define MINS(t) SECS(t) * 60
+    #define HOURS(t) MINS(t) * 60
+    #define STOP 0
+
+    class Timer
+    {
+    private:
+        uint32_t _time;
+        uint32_t _lastTime;
+
+    public:
+        void begin(const uint32_t);
+        void restart();
+        bool available();
+        void time(const uint32_t);
+        void add(const uint32_t);
+        uint32_t time();
+    };
+
+#endif
